f19eb8789dbe1be88fabcaf1ad36dc981945ed47
Jeremy Garnes <J06Garnes@gmail.com>
2020-11-18 00:18:07 -0500

    Changed Image

--- src/Components/headerComponent.js
+++ src/Components/headerComponent.js
@@ -8,7 +8,7 @@ function Header () {
         <Container fluid className="back Hcontain" id="home">
             <Row className="inner move">
                 <Col > 
-                    <h1 className="Hh1">Hello, My name is Jeremy Garnes</h1>
+                    <h1 className="Hh1">My name is Jeremy Garnes</h1>
                     <h2 className="Hh2">I am a Full Stack Web-developer</h2>
                     
                 </Col>
